da1cd54d6980263a5ed01da2529ad115143503c2
a_szczepanik <a.szczepanik@adform.com>
2022-03-23 13:48:45 +0100

    "baked a cake, lighted a candle"

--- src/main/scala/StackedBoxes.scala
+++ src/main/scala/StackedBoxes.scala
@@ -1,8 +1,9 @@
 object StackedBoxes extends App {
   val ls = args.sortBy(_.length)
   val maxL = ls(ls.length - 1).length
-  println(" "*((maxL/2)-1) + "*")
-  println(" "*((maxL/2)-1) + "|")
+  println(" " * ((maxL / 2) - 1) + "*" * 3)
+  println(" " * ((maxL / 2) - 1) + "|" * 3)
+
   def boxWord(): Unit = {
     ls.foreach(l => {
       var spaceN = (maxL - l.length) / 2
