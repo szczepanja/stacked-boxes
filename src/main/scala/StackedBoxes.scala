7b7b87c9f59491d52c4bbf0b7e305dc75b25d100
a_szczepanik <a.szczepanik@adform.com>
2022-03-23 13:37:50 +0100

    "baked a cake"

---
+++ src/main/scala/StackedBoxes.scala
@@ -0,0 +1,22 @@
+object StackedBoxes extends App {
+  val ls = args.sortBy(_.length)
+  val maxL = ls(ls.length - 1).length
+
+  def boxWord(): Unit = {
+    ls.foreach(l => {
+      var spaceN = (maxL - l.length) / 2
+
+      if (ls.indexOf(l) == ls.length - 1) {
+        println("+" + "-" * (l.length) + "+")
+        println(" " * spaceN + "|" + l + "|")
+        println("+" + "-" * (l.length) + "+")
+      } else {
+        println(" " * spaceN + "+" + "-" * (l.length) + "+")
+        println(" " * spaceN + "|" + l + "|")
+      }
+    })
+  }
+
+  boxWord()
+}
+
